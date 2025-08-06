ad5eafbb4cd8e2b9a72cef187f80942e615e17cc
danaeswart <241022@virtualwindow.co.za>
2025-08-06 17:26:50 +0200

    Login and signup cread

---
+++ the-bloom-room/src/pages/Login.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./css/Login.css"; // Assuming you have a CSS file for styling
+import { Link } from "react-router-dom"; // Import Link at the top
+
+import FlowerBackground from "../components/FlowerBackground"; // Import the FlowerBackground component
+function Login() {
+  const [role, setRole] = useState("artist");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Role:", role, "Email:", email, "Password:", password);
+    // Handle login logic here
+  };
+
+  return (
+    <>
+    <FlowerBackground />{/* Render the flower background */}
+    <div className="login-container">
+      
+      <h2 className="login-title">Login</h2>
+
+            {/* Role selection */}
+        <div className="role-selection">
+        <div className="role-option"  onClick={() => setRole("admin")}>
+            <button
+            className={`role-button ${role === "admin" ? "active" : ""}`}
+            
+            type="button"
+            >
+            As Admin
+            </button>
+        </div>
+        <div className="role-option" onClick={() => setRole("artist")}>
+            <button
+            className={`role-button ${role === "artist" ? "active" : ""}`}
+            
+            type="button"
+            >
+            As Artist
+            </button>
+        </div>
+        <div className="role-option"   onClick={() => setRole("buyer")}>
+            <button
+            className={`role-button ${role === "buyer" ? "active" : ""}`}
+            
+            type="button"
+            >
+            As Buyer
+            </button>
+        </div>
+        </div>
+
+
+      {/* Login form */}
+      <form className="login-form" onSubmit={handleSubmit}>
+       <label className="login-label">Email:</label>
+        <input
+          type="email"
+          placeholder="Email"
+          className="login-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+         <label className="login-label">Password:</label>
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" className="login-button">Login</button>
+        <p className="signup-text">
+    Don't have an account?{" "}
+    <Link to="/signup" className="signup-link">
+      Sign Up
+    </Link>
+  </p>
+      </form>
+    </div>
+     </>
+  );
+}
+
+export default Login;
