b428340c9dd4fd002562320e36767512df5032ef
danaeswart <241022@virtualwindow.co.za>
2025-08-07 11:58:32 +0200

    wireframes ajusted

--- the-bloom-room/src/components/FlowerBackground.js
+++ the-bloom-room/src/components/FlowerBackground.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import flowerBlue from "../assets/images/flower-blue.png";
 import flowerGreen from "../assets/images/flower-green.png";
 import flowerPink from "../assets/images/flower-pink.png";
@@ -6,22 +6,6 @@ import flowerRed from "../assets/images/flower-red.png";
 import "./css/FlowerBackground.css";
 
 function FlowerBackground() {
-  useEffect(() => {
-    const handleScroll = () => {
-      const flowers = document.querySelectorAll(".flower");
-      flowers.forEach((flower) => {
-        if (window.scrollY > 50) {
-          flower.classList.add("spin-up");
-        } else {
-          flower.classList.remove("spin-up");
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="flower-background-component">
       <img src={flowerBlue} alt="Flower 1" className="flower flower-top-left" />
--- the-bloom-room/src/pages/Home.js
+++ the-bloom-room/src/pages/Home.js
@@ -4,12 +4,22 @@ import Navbar from "../components/Navbar";
 import PostContainer from "../components/PostContainer"; // <-- Ensure this exists
 import logo from "../assets/images/logo.png";
 import "./css/Home.css";
+import flower1 from "../assets/images/flower-red.png"; 
+import flower2 from "../assets/images/flower-blue.png"; 
+import flower3 from "../assets/images/flower-green.png"; 
+import flower4 from "../assets/images/flower-lightgreen.png"; // Example image, ensure this exists
 
 const Home = () => {
   return (
     <>
-      <FlowerBackground scroll={true} />
+      {/* <FlowerBackground scroll={true} /> */}
       <Navbar />
+      <div className="home-background">
+        <img src={flower1} alt="Flower 1" className="flower flower-top-left" />
+        <img src={flower2} alt="Flower 2" className="flower flower-top-right" />
+        <img src={flower3} alt="Flower 3" className="flower flower-bottom-left" />
+        <img src={flower4} alt="Flower 4" className="flower flower-bottom-right" />
+        </div>
       <div className="home-content">
         <h1>Welcome to</h1>
         <img src={logo} alt="Logo" className="home-logo" />
